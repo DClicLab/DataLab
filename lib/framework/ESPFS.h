c70b66fd6b1022ce4b222835d9be49b1d73f84e8
Raomin <4460208+raomin@users.noreply.github.com>
2021-01-22 23:49:58 +0100

    supports esp32 littleFS

--- lib/framework/ESPFS.h
+++ lib/framework/ESPFS.h
@@ -1,6 +1,8 @@
 #ifdef ESP32
-#include <SPIFFS.h>
-#define ESPFS SPIFFS
+// #include <SPIFFS.h>
+// #define ESPFS SPIFFS
+#include <LittleFS.h>
+#define ESPFS LITTLEFS
 #elif defined(ESP8266)
 #include <LittleFS.h>
 #define ESPFS LittleFS
